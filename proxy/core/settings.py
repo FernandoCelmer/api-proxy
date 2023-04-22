c5ae6bc92e95b30066d96236ca3ca412cc175ce7
FernandoCelmer <email@fernandocelmer.com>
2023-04-22 07:50:28 -0300

    ⚙️ FEATURE: Update Settings

--- proxy/core/settings.py
+++ proxy/core/settings.py
@@ -1,19 +1,34 @@
+from json import loads
 from logging import info
 from os import environ, path
 
+from proxy.core.file import File
 
-def set_up():
-    """Sets up configuration for the app
-    """
-    try:
-        from dotenv import load_dotenv
-        load_dotenv()
-    except Exception as error:
-        info(str(error))
 
-    config = {
-        "DEBUG": bool(environ.get("DEBUG", 1)),
-        "ENVIRONMENT": environ.get("ENVIRONMENT", 'development'),
-        "BASE_DIR": path.abspath(path.dirname(__file__))
-    }
-    return config
+class Settings:
+
+    def __init__(self) -> None:
+        try:
+            from dotenv import load_dotenv
+            load_dotenv()
+        except Exception as error:
+            info(str(error))
+
+    @staticmethod
+    def load():
+        """Sets up configuration for the app
+        """
+
+        variables = {
+            "DEBUG": bool(environ.get("DEBUG", 1)),
+            "ENVIRONMENT": environ.get("ENVIRONMENT", 'development'),
+            "DATABASE_URL": environ.get("DATABASE_URL", "sqlite:///./sql_app.db"),
+            "BASE_DIR": path.abspath(path.dirname(__file__)),
+            "PROXY": loads(File.read(path_file="./proxy/data/proxy.json")),
+            "USERS": loads(File.read(path_file="./proxy/data/users.json"))
+        }
+
+        for config in variables:
+            globals()[config] = variables[config]
+
+        return variables
