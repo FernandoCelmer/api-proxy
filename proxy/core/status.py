7a0a79dec6c83fb2db8a8f1ff0d9862245040424
FernandoCelmer <email@fernandocelmer.com>
2023-04-22 20:04:04 -0300

    ⚙️ FEATURE: Functions for status code validation

---
+++ proxy/core/status.py
@@ -0,0 +1,18 @@
+def is_informational(code):
+    return 100 <= code <= 199
+
+
+def is_success(code):
+    return 200 <= code <= 299
+
+
+def is_redirect(code):
+    return 300 <= code <= 399
+
+
+def is_client_error(code):
+    return 400 <= code <= 499
+
+
+def is_server_error(code):
+    return 500 <= code <= 599
