38b4ee0a18c8de02e6b38966a5362f08fa83713f
FernandoCelmer <email@fernandocelmer.com>
2023-04-22 20:05:50 -0300

    ⚙️ FEATURE: New route to show connection information

--- proxy/api/v1/routers.py
+++ proxy/api/v1/routers.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
-from proxy.api.v1.endpoints import proxy, metrics
+from proxy.api.v1.endpoints import proxy, metrics, client
 
 router = APIRouter()
+
+
 router.include_router(proxy.router, tags=["Proxy"])
 router.include_router(metrics.router, tags=["Metrics"])
+router.include_router(client.router, tags=["Client"])
