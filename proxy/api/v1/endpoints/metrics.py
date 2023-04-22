1763b409cf5249f53d26209964d09ec2e09060a0
FernandoCelmer <email@fernandocelmer.com>
2023-04-22 02:10:15 -0300

    ⚙️ FEATURE: Endpoint responsible for showing metrics

---
+++ proxy/api/v1/endpoints/metrics.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from fastapi.responses import PlainTextResponse
+from prometheus_client import generate_latest
+
+router = APIRouter()
+
+
+@router.get("/metrics", include_in_schema=False)
+async def get_metrics():
+    return PlainTextResponse(generate_latest())
