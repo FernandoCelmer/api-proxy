c9cd8fd68f0928c4064ad6db35cd89011f8a8790
FernandoCelmer <email@fernandocelmer.com>
2023-04-22 20:15:13 -0300

    ⚙️ FEATURE: Initial implementation to validate users

--- proxy/core/request.py
+++ proxy/core/request.py
@@ -1,12 +1,13 @@
 """
 Module File
 """
-import logging
 
 from json import dumps
 from fastapi.responses import JSONResponse, Response
 from requests import request, HTTPError
 from urllib.parse import urlsplit, parse_qs
+
+from proxy.core.status import is_server_error
 from proxy.core.settings import Settings
 
 
@@ -14,11 +15,15 @@ class BaseRequest:
 
     def __init__(self, request) -> None:
         self.request = request
+
         self.client = {}
         self.proxy = {}
+        self.path = {}
+
         self.method = None
         self.url_proxy = None
         self.url_target = None
+
         self.params = {}
         self.body = {}
         self.headers = {}
@@ -41,15 +46,16 @@ class BaseRequest:
     def setup_url_target(self):
         for client in self.client:
             for path in client.get("paths"):
-                if self.url_proxy.path == path:
-                    self.proxy = self.config["PROXY"].get(client.get("name"))
+                if self.url_proxy.path == path.get("path"):
+                    self.proxy = self.config["PROXY"].get(client.get("host"))
+                    self.path = path
                     break
 
         if self.proxy:
             self.url_target = "{scheme}://{host}{path}".format(
                 scheme=self.proxy.get("scheme"),
                 host=self.proxy.get("host"),
-                path=path
+                path=path.get("path")
             )
 
     def setup_params(self):
@@ -88,6 +94,10 @@ class BaseRequest:
         self.setup_params()
         self.setup_method()
 
+    def validate(self):
+        if not self.path:
+            return Response(status_code=401)
+
     async def setup_body(self):
         try:
             return await self.request.json()
@@ -95,6 +105,9 @@ class BaseRequest:
             pass
 
     async def make_request(self, max_retry: int = 5, retry: int = 0):
+        if self.validate():
+            return self.validate()
+
         kwargs = {
             'method': self.method,
             'url': self.url_target,
@@ -106,18 +119,23 @@ class BaseRequest:
         try:
             with request(**kwargs) as response:
                 base_response = response
-                try:
-                    response.raise_for_status()
-                except HTTPError:
-                    if retry < max_retry:
-                        retry += 1
-                        base_response = await self.make_request(
-                            retry=retry
-                        )
 
-            return JSONResponse(
-                content=base_response.json(),
-                status_code=base_response.status_code)
+                if is_server_error(base_response.status_code):
+                    try:
+                        response.raise_for_status()
+                    except HTTPError:
+                        if retry < max_retry:
+                            retry += 1
+                            base_response = await self.make_request(
+                                retry=retry
+                            )
+            try:
+                return JSONResponse(
+                    content=base_response.json(),
+                    status_code=base_response.status_code)
+            except Exception:
+                return Response(
+                    status_code=base_response.status_code)
 
         except Exception:
             return Response(status_code=422)
