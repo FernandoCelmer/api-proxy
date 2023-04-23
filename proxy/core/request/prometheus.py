42b4755f7d09f514ba96a5bf630d5de918f53102
FernandoCelmer <email@fernandocelmer.com>
2023-04-23 01:03:50 -0300

    ⚙️ FEATURE: Add RequestPrometheus

---
+++ proxy/core/request/prometheus.py
@@ -0,0 +1,35 @@
+"""
+Module Request Prometheus
+"""
+
+from proxy.core.request.base import BaseRequest
+from proxy.core.settings import Settings
+
+
+class RequestPrometheus(BaseRequest):
+
+    def __init__(self, request=None) -> None:
+        self.config = Settings.load_variables()
+        super().__init__(request)
+
+    def setup_method(self):
+        self.method = 'GET'
+
+    def setup_url_target(self):
+        url_prometheus = self.config.get("URL_PROMETHEUS")
+        self.url_target = f"{url_prometheus}/api/v1/query"
+
+    def setup_params(self, client=None, path=None):
+        query = f"get_total[job='proxy-api', client='{client}', path='{path}']"
+        self.params = {
+            "query": query.replace("[", "{").replace("]", "}")
+        }
+
+    def setup_headers(self):
+        self.headers = {
+            "content-type": "application/json"
+        }
+
+    async def prometheus_request(self, client: str, path: str):
+        self.setup_params(client=client, path=path)
+        return await self.make_request()
