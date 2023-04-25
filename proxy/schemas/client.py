56e21c8de42c85772aae31134153d2530b6a1fc9
FernandoCelmer <email@fernandocelmer.com>
2023-04-25 00:00:19 -0300

    📌 ISSUE-#1: Update Schemas

--- proxy/schemas/client.py
+++ proxy/schemas/client.py
@@ -1,29 +1,27 @@
+from typing import List
 from bson import ObjectId
 from typing import Optional
 from pydantic import BaseModel, Field
 from proxy.core.database.mongodb import PyObjectId
 
 
-class SchemaBase(BaseModel):
+class SchemaClientPaths(BaseModel):
+    path: str
+    max_request: int
+    time_stamp: int
+
+
+class SchemaClientBase(BaseModel):
     user: str
     host: str
-    paths: Optional[list] = []
-
-class SchemaCreate(SchemaBase):
-    pass
-
-
-class SchemaUpdate(SchemaBase):
-    user: Optional[str]
-    host: Optional[str]
-    paths: Optional[list]
+    paths: Optional[List[SchemaClientPaths]] = []
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
 
 
-class Schema(SchemaBase):
+class SchemaClient(SchemaClientBase):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
 
     class Config:
--- proxy/schemas/proxy.py
+++ proxy/schemas/proxy.py
@@ -1,29 +1,28 @@
+from typing import List
 from bson import ObjectId
 from typing import Optional
 from pydantic import BaseModel, Field
 from proxy.core.database.mongodb import PyObjectId
 
 
-class SchemaBase(BaseModel):
+class SchemaProxyPaths(BaseModel):
+    path: str
+    target: str
+    max_request: int
+    time_stamp: int
+
+
+class SchemaProxyBase(BaseModel):
     host: str
     scheme: str
-    paths: Optional[list] = []
-
-class SchemaCreate(SchemaBase):
-    pass
-
-
-class SchemaUpdate(SchemaBase):
-    host: Optional[str]
-    scheme: Optional[str]
-    paths: Optional[list]
+    paths: Optional[List[SchemaProxyPaths]] = []
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
 
 
-class Schema(SchemaBase):
+class SchemaProxy(SchemaProxyBase):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
 
     class Config:
