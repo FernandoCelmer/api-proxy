c39848639697eea72235b79683d0f268021e93c8
FernandoCelmer <email@fernandocelmer.com>
2023-04-30 02:24:34 -0300

    📌 ISSUE-#7: Create User and Endpoint Validation with MongoDB

--- proxy/api/v1/endpoints/redirect.py
+++ proxy/api/v1/endpoints/redirect.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter, Request, Response, status
 from proxy.core.request.proxy import RequestProxy
-from proxy.core.error import ErrorProcessProxy
-# from proxy.core.storage import Storage
+from proxy.core.error import (
+    ErrorSetupStorage,
+    ErrorSetupValidate,
+    ErrorManyRequests
+)
+
+from proxy.core.handler import Handler
+from proxy.core.validate import Validate
 
 router = APIRouter()
 
@@ -10,52 +16,107 @@ router = APIRouter()
 async def proxy(request: Request):
     """GET Proxy"""
     try:
-        # data = await Storage(request=request).build()
-        return await RequestProxy(request).get()
-    except ErrorProcessProxy:
+        storage = await Handler(request=request).setup()
+        await Validate(**storage).setup()
+
+        return await RequestProxy(**storage).get()
+
+    except ErrorSetupStorage:
         return Response(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorSetupValidate:
+        return Response(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorManyRequests:
+        return Response(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS)
 
 
 @router.post("/redirect", include_in_schema=False)
 async def post_proxy(request: Request):
     """POST Proxy"""
     try:
-        # data = await Storage(request=request).build()
-        return await RequestProxy(request).post()
-    except ErrorProcessProxy:
+        storage = await Handler(request=request).setup()
+        await Validate(**storage).setup()
+
+        return await RequestProxy(**storage).post()
+
+    except ErrorSetupStorage:
         return Response(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorSetupValidate:
+        return Response(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorManyRequests:
+        return Response(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS)
 
 
 @router.put("/redirect", include_in_schema=False)
 async def put_proxy(request: Request):
     """PUT Proxy"""
     try:
-        # data = await Storage(request=request).build()
-        return await RequestProxy(request).put()
-    except ErrorProcessProxy:
+        storage = await Handler(request=request).setup()
+        await Validate(**storage).setup()
+
+        return await RequestProxy(**storage).put()
+
+    except ErrorSetupStorage:
         return Response(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorSetupValidate:
+        return Response(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorManyRequests:
+        return Response(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS)
 
 
 @router.patch("/redirect", include_in_schema=False)
 async def patch_proxy(request: Request):
     """PATCH Proxy"""
     try:
-        # data = await Storage(request=request).build()
-        return await RequestProxy(request).patch()
-    except ErrorProcessProxy:
+        storage = await Handler(request=request).setup()
+        await Validate(**storage).setup()
+
+        return await RequestProxy(**storage).patch()
+
+    except ErrorSetupStorage:
         return Response(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorSetupValidate:
+        return Response(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorManyRequests:
+        return Response(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS)
 
 
 @router.delete("/redirect", include_in_schema=False)
 async def delete_proxy(request: Request):
     """DELETE Proxy"""
     try:
-        # data = await Storage(request=request).build()
-        return await RequestProxy(request).delete()
-    except ErrorProcessProxy:
+        storage = await Handler(request=request).setup()
+        await Validate(**storage).setup()
+
+        return await RequestProxy(**storage).delete()
+
+    except ErrorSetupStorage:
         return Response(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorSetupValidate:
+        return Response(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    except ErrorManyRequests:
+        return Response(
+            status_code=status.HTTP_429_TOO_MANY_REQUESTS)
